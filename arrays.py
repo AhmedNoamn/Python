d22d7e48f999caec1fa643d5e188883b53fea249
AhmedNoamn <Ahmedfayo9@gmail.com>
2022-09-02 16:27:50 +0200

    comment and variables

---
+++ arrays.py
@@ -0,0 +1,5 @@
+list_name = ['ahmed', 'nimo', 'memo']
+list_name.append('mido')
+list_name.remove('mido')
+
+print(list_name)
--- variables.py
+++ variables.py
@@ -1,7 +1,17 @@
-import random
-
+"""import random
 print(random.randrange(1, 10))
+"""
+# casting     التحويل
+"""
+x1 = 5
+y = 1.5
+print(int(y))
+print(float(x1))
+x2='5'
+x3='7'
+print(int(x2)+int(x3))
 
+"""
 # line  comment
 """
 multi line comments

c584f56926e6b34e75b409d7a56848d9f1112592
AhmedNoamn <Ahmedfayo9@gmail.com>
2022-09-02 15:58:58 +0200

    comment and variables

--- variables.py
+++ variables.py
@@ -1,12 +1,18 @@
+import random
+
+print(random.randrange(1, 10))
+
 # line  comment
 """
 multi line comments
 """
 
 # variables
+"""
 x = 2
 y = 2.5
 name = 'ahmed'
 num = int
 num = 5
 print(num)
+"""

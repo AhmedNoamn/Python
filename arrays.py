6b0bdac2cd6593c4960613d1198467fcd79c0f2d
AhmedNoamn <Ahmedfayo9@gmail.com>
2022-09-04 21:11:45 +0200

    arrays

--- arrays.py
+++ arrays.py
@@ -1,5 +1,27 @@
+"""
 list_name = ['ahmed', 'nimo', 'memo']
 list_name.append('mido')
 list_name.remove('mido')
 
 print(list_name)
+
+#tuple means that youi can not modfy it
+list_name=('ahmed','mido')
+del list_name
+print(len(list_name))
+# sets pop it delete the first element
+# add it adds in the first not the last
+list_set = {'ahmed', 2, 'nimo'}
+list_set.add('gana')
+list_set.pop()
+print(list_set)
+"""
+# dictionary or map
+
+list_map = {
+    '12': 'asmaa',
+    '13': 'mano',
+    '14': 'aya',
+    '15': 'hamoo',
+}
+print(list_map['14'])
